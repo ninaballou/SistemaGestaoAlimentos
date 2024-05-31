5c4fd268c45f7b28514b81524309ef37299ab55b
Hayna Lopes <115355378+ninaballou@users.noreply.github.com>
2024-05-31 13:24:39 +0000

    Adicionado função para geração de número aleatório de pedido.

--- script.js
+++ script.js
@@ -18,8 +18,14 @@ function registrarDataTransacao(){
  return new Date().toLocaleString() ;
 }
 
+function gerarNumeroTransacao() {
+  // RN 0.5 geração de numero de transação unico
+  return Math.floor(Math.random()*10000);
+}
+
 function registrarVenda(produto, quantidade, precoUnitario){
     let dataTransacao = registrarDataTransacao(); 
+    let numeroTransacao = gerarNumeroTransacao();
 
   let validaCamposObrigatorios = validarCamposObrigatorios(produto, quantidade, precoUnitario, dataTransacao);
   let validaQuantidadeProdutos = validarQuantidadeProdutos(quantidade)
@@ -28,7 +34,7 @@ function registrarVenda(produto, quantidade, precoUnitario){
   if(validaCamposObrigatorios){
     if(validaQuantidadeProdutos){
       if(validaPrecoUnitario){
-      alert("Transação realizada com sucesso! Produto: " + produto + " ,Quantidade: " + quantidade + " Preço Unitário:R$ " + precoUnitario + " Data da Transação: " + dataTransacao);
+      alert("["+numeroTransacao+"] Transação reagistrada! Produto: " + produto + " ,Quantidade: " + quantidade + " Preço Unitário:R$ " + precoUnitario + " Data da Transação: " + dataTransacao);
     } else {
       alert("Problemas na Validação de Quantidade de Produtos!")   
     }
@@ -39,6 +45,5 @@ function registrarVenda(produto, quantidade, precoUnitario){
 }
 
 registrarVenda("arroz", 5 , 10);
-registrarVenda("feijão", 20, 25);
-registrarVenda("farofa", 10, 3, 40);
+
 
